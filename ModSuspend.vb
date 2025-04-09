6149d7781e70628f7a77fe9f951f7dc9f6a7f2f5
therealhach <133066170+therealhach@users.noreply.github.com>
2025-04-09 14:13:45 +0800

    Add files via upload

---
+++ ModSuspend.vb
@@ -0,0 +1,42 @@
+﻿Module ModSuspend
+    Public Enum ThreadAccess As Integer
+        TERMINATE = (&H1)
+        SUSPEND_RESUME = (&H2)
+        GET_CONTEXT = (&H8)
+        SET_CONTEXT = (&H10)
+        SET_INFORMATION = (&H20)
+        QUERY_INFORMATION = (&H40)
+        SET_THREAD_TOKEN = (&H80)
+        IMPERSONATE = (&H100)
+        DIRECT_IMPERSONATION = (&H200)
+    End Enum
+
+    Public Declare Function OpenThread Lib "kernel32.dll" (ByVal dwDesiredAccess As ThreadAccess, ByVal bInheritHandle As Boolean, ByVal dwThreadId As UInteger) As IntPtr
+    Public Declare Function SuspendThread Lib "kernel32.dll" (ByVal hThread As IntPtr) As UInteger
+    Public Declare Function ResumeThread Lib "kernel32.dll" (ByVal hThread As IntPtr) As UInteger
+    Public Declare Function CloseHandle Lib "kernel32.dll" (ByVal hHandle As IntPtr) As Boolean
+    Public Declare Function Sleep Lib "kernel32" (ByVal MilliSecond As Long) As Long
+
+    Public Sub SuspendProcess(ByVal process As System.Diagnostics.Process)
+        For Each t As ProcessThread In process.Threads
+            Dim th As IntPtr
+            th = OpenThread(ThreadAccess.SUSPEND_RESUME, False, t.Id)
+            If th <> IntPtr.Zero Then
+                SuspendThread(th)
+                CloseHandle(th)
+            End If
+        Next
+    End Sub
+
+
+    Public Sub ResumeProcess(ByVal process As System.Diagnostics.Process)
+        For Each t As ProcessThread In process.Threads
+            Dim th As IntPtr
+            th = OpenThread(ThreadAccess.SUSPEND_RESUME, False, t.Id)
+            If th <> IntPtr.Zero Then
+                ResumeThread(th)
+                CloseHandle(th)
+            End If
+        Next
+    End Sub
+End Module
